2ee6dd9b9f0f19a5ebad55a57b10c485a88e3d7d
samuelVJrobinson <samuel.vj.robinson@gmail.com>
2020-01-13 18:52:43 -0700

    Started to work on extracting wetland distances

--- prelimAnalysis.R
+++ prelimAnalysis.R
@@ -9,26 +9,29 @@ library(sf)
 
 # Basic maps of sites/trap distribution -----------------------------------
 
+#Calgary-centred projection from Paul - seems to be problems when using this
+abProjString <- '+proj=tmerc +lat_0=0 +lon_0=-114 +k=0.9999 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs'
+
 #Provincial map
-ab <- st_read('~/Documents/shapefiles/provBorders') %>% #Read in provincial borders
+ab <- st_read('~/Documents/shapefiles/provBorders/provBorders.shp') %>% #Read in provincial borders
   filter(PRNAME=='Alberta') %>% #Alberta only
-  st_transform(3403) #Transform to NAD83, UTM 11N (EPSG:2153), or try EPSG:3403  
+  st_transform(3403) #Transform to NAD83, UTM 11N (EPSG:2153), or try EPSG:3403
+  # st_transform(abProjString) #Custom projection from Paul
+# st_write(ab,'~/Documents/shapefiles/provBorders/AB_only.shp',driver='ESRI Shapefile',delete_layer=T)
 
 #Cities
-cit <- st_read('~/Documents/shapefiles/ABcities') %>% filter(PLACE=='city') %>% 
+cit <- st_read('~/Documents/shapefiles/ABlocations/alberta_location.shp') %>% 
+  filter(PLACE=='city'|NAME=='Vermilion') %>% 
   filter(!is.na(NAME),NAME!='Airdrie',NAME!='Delia',NAME!='Cold Lake') %>% 
   st_transform(3403) 
-# st_write(cit,'~/Documents/shapefiles/ABcities',driver='ESRI Shapefile')
+# st_write(cit,'~/Documents/shapefiles/ABcities',driver='ESRI Shapefile',delete_layer=T)
 
 #City labels of interest
 citLabs <- cit %>% 
-  filter(NAME %in% c('Grande Prairie','Edmonton','Red Deer','Calgary','Brooks','Lethbridge')) %>%
+  filter(NAME %in% c('Grande Prairie','Edmonton','Red Deer','Calgary','Brooks','Lethbridge','Vermilion')) %>%
   # mutate(NAME=gsub(' ','\n',NAME)) %>% 
   select(NAME,geometry) %>% 
-  mutate(xnudge=c(8e4,0,-6e4,0,-6e4,4e4),ynudge=c(0,2e4,0,2e4,0,-2e4)) #Distances to nudge city labels
-
-#Bounding box for 2016:2018 sites (excludes Grande Prairie)
-bounds <- st_bbox(filter(trapSf,startYear!=2015))
+  mutate(xnudge=c(7e4,0,-5e4,0,-5e4,4e4,-2e4),ynudge=c(0,2e4,0,2e4,0,0,2e4)) #Distances to nudge city labels
 
 #Add coordinate system to site df
 siteSf <- st_as_sf(site,coords=c('lon','lat'),crs=4326) %>% 
@@ -41,19 +44,22 @@ trapSf <- trap %>% left_join(select(site,BLID:lon),by='BLID') %>%
   mutate(latTrap=ifelse(latTrap==0,latSite,latTrap),lonTrap=ifelse(lonTrap==0,lonSite,lonTrap)) %>% 
   st_as_sf(coords=c('lonTrap','latTrap'),crs=4326) %>% 
   st_transform(3403)
+
+#Bounding box for 2016:2018 sites (excludes Grande Prairie)
+bounds <- st_bbox(filter(trapSf,startYear!=2015))
   
 #Overall Site distributions
-p1 <- ggplot(siteSf)+ geom_sf(size=0.3)+
+(p1 <- ggplot(siteSf)+ geom_sf(size=0.3)+
   geom_sf_text(data=citLabs,aes(label=NAME),col='red',size=3,
                nudge_y=citLabs$ynudge,nudge_x=citLabs$xnudge)+
   geom_sf(data=citLabs,col='red',size=1)+
   geom_sf(data=ab,fill=NA) + 
   coord_sf(xlim=st_bbox(siteSf)[c(1,3)],ylim=st_bbox(siteSf)[c(2,4)])+
-  ggtitle('Overall site distributions')+labs(x=NULL,y=NULL)
-ggsave('./figures/maps/overallMap.png',p1)
+  ggtitle('Overall site distributions')+labs(x=NULL,y=NULL))
+# ggsave('./figures/maps/overallMap.png',p1)
 
 #Trapping type distribution
-p1 <- trapSf %>% mutate(startYear=factor(startYear)) %>% 
+(p1 <- trapSf %>% mutate(startYear=factor(startYear)) %>% 
   ggplot()+ geom_sf(size=0.3)+
   # geom_sf_text(data=citLabs,aes(label=NAME),col='red',size=3,
                # nudge_y=citLabs$ynudge,nudge_x=citLabs$xnudge)+
@@ -62,22 +68,22 @@ p1 <- trapSf %>% mutate(startYear=factor(startYear)) %>%
   geom_sf(data=cit,col='red',size=1)+
   coord_sf(xlim=bounds[c(1,3)],ylim=bounds[c(2,4)])+
   ggtitle('Trap type distributions')+
-  theme(axis.text=element_text(size=5),axis.text.x=element_text(angle=90,vjust=0.3,hjust=1))
-ggsave('./figures/maps/trapTypeMap.png',p1)
+  theme(axis.text=element_text(size=5),axis.text.x=element_text(angle=90,vjust=0.3,hjust=1)))
+# ggsave('./figures/maps/trapTypeMap.png',p1)
 
 #Non-ditch trapping (none from 2015)
 nonDitch <- trapSf %>% mutate(startYear=factor(startYear)) %>% 
   filter(trapLoc!='ditch')
 
-p1 <- ggplot(nonDitch)+ geom_sf(size=0.3)+
+(p1 <- ggplot(nonDitch)+ geom_sf(size=0.3)+
   facet_grid(trapType~startYear)+
   geom_sf(data=ab,fill=NA)+
   geom_sf(data=cit,col='red',size=1)+
   # geom_sf_text(data=citLabs,aes(label=NAME),col='red',size=3,nudge_y=citLabs$ynudge,nudge_x=citLabs$xnudge)+
   coord_sf(xlim=bounds[c(1,3)],ylim=bounds[c(2,4)]) + 
   ggtitle('Wetland & in-field trap distributions')+
-  theme(axis.text=element_text(size=5),axis.text.x=element_text(angle=90,vjust=0.3,hjust=1))
-ggsave('./figures/maps/nonDitchMap.png',p1)
+  theme(axis.text=element_text(size=5),axis.text.x=element_text(angle=90,vjust=0.3,hjust=1)))
+# ggsave('./figures/maps/nonDitchMap.png',p1)
 
 #Maps of arthropod groups (arthOrder) used to check completeness
 arthNumbers <- arth %>% filter(!mismatchBTID,!mismatchBLID) %>% 
@@ -86,17 +92,17 @@ arthNumbers <- arth %>% filter(!mismatchBTID,!mismatchBLID) %>%
 yearNumbers <- arthNumbers %>% 
   group_by(BLID,year) %>% summarize(n=sum(n)) %>% 
   left_join(select(siteSf,BLID),by='BLID') 
+  
 
 #Arth maps
-p1 <- ggplot()+ geom_sf(data=yearNumbers,aes(geometry=geometry),size=0.3,shape=1,alpha=0.3)+
+(p1 <- ggplot()+ geom_sf(data=yearNumbers,aes(geometry=geometry),size=0.3,shape=1,alpha=0.3)+
   geom_sf(data=arthNumbers,aes(geometry=geometry),size=0.3)+
   geom_sf(data=cit,col='red',size=1)+ #Cities
   facet_grid(arthOrder~year)+
   coord_sf(xlim=bounds[c(1,3)],ylim=bounds[c(2,4)])+
   ggtitle('Sampling and occurance distribution')+
-  theme(axis.text=element_text(size=5),axis.text.x=element_text(angle=90,vjust=0.3,hjust=1))
-ggsave('./figures/maps/arthDistMap.png',p1,scale=1.5)
-
+  theme(axis.text=element_text(size=5),axis.text.x=element_text(angle=90,vjust=0.3,hjust=1)))
+  # ggsave('./figures/maps/arthDistMap.png',p1,scale=1.5)
 
 # Create new shapefiles for 2018 data --------------------------------
 
@@ -105,16 +111,48 @@ for(i in c(2015,2016,2018)){
   trapSf %>% filter(startYear==i,trapLoc=='ditch') %>% 
     select(BLID,replicate,trapType) %>% unique() %>% 
     st_transform(3403) %>% 
-    st_write(paste0('~/Documents/shapefiles/DitchSites_',i,'.shp'),delete_dsn=TRUE)
+    st_write(paste0('~/Documents/shapefiles/digitizedFeaturesSR/DitchSites_',i,'.shp'),delete_dsn=TRUE)
 }
+
 #Add infields from 2018
 trapSf %>% filter(startYear==2018,trapLoc!='ditch') %>% 
   select(BLID,replicate,trapType) %>% unique() %>% 
   st_transform(3403) %>% 
-  st_write('~/Documents/shapefiles/InfieldSite_2018.shp',delete_dsn=TRUE)
+  st_write('~/Documents/shapefiles/digitizedFeaturesSR/InfieldSites_2018.shp',delete_dsn=TRUE)
+
+#Get various shapefiles from GDB (can't be written to outside of ArcMap), transform, and save in another folder
+layers <- st_layers('~/Documents/shapefiles/DigitizedFeatures_AllYears.gdb')$name
+layers <- layers[!grepl('Buffer',layers)] #Get rid of buffer layers
+  
+for(i in layers){
+  j <- st_read(dsn='~/Documents/shapefiles/DigitizedFeatures_AllYears.gdb',layer=i) %>% 
+    st_zm() %>% st_transform(3403)
+  st_write(j,paste0('~/Documents/shapefiles/digitizedFeaturesSR/',i,'.shp'),driver='ESRI Shapefile',delete_layer=TRUE)
+}
 
 #Only 16 of the ditch sites from 2018 (repeats of previous years in S. Calgary) have been digitized.
 #The remaining ditch sites (41) and infields (27) need wetland layers created around them
+
+#Get wetland distances from various infield sites
+
+#Wetland distances 
+wtrap2016 <- trapSf %>% filter(startYear==2016,grepl('W',replicate)) #trap locations
+wland2016 <- st_read(dsn='~/Documents/shapefiles/digitizedFeaturesSR/Wetlands2016.shp') #wetland polygons
+
+#Buffer all unique traps by 1km
+trapBuff <- wtrap2016 %>% select(BLID) %>% unique() %>% st_buffer(dist=1000)
+
+#Filter wetlands that overlap buffer distance
+wland2016 <- wland2016[sort(unique(unlist(st_intersects(trapBuff,wland2016)))),]
+
+wland2016[unique(st_nearest_feature(wtrap2016,wland2016)),'isNearest'] <- TRUE
+
+#Appears to work
+ggplot()+geom_sf(data=wland2016,aes(fill=isNearest))+
+  geom_sf(data=wtrap2016,col='blue')
+
+
+
 
 # Question 1: Spillover into crops ----------------------------------------
 
