f941e3cb25ccf286223e0448bdad4432fa13c8e5
samuelVJrobinson <samuel.vj.robinson@gmail.com>
2020-01-16 17:18:23 -0700

    Worked on dispersal simulation

--- dispersalSim.R
+++ dispersalSim.R
@@ -10,10 +10,24 @@ ntPlus <- function(nt,r,K){
   nt+(r*nt*(1-(nt/K)))
 }
 
+# expCDF <- function(x,alpha) 1-exp(-alpha*x) #Exponential curve
+# invExpCDF <- function(x,alpha) -log(-x+1)/alpha
+# curve(qexp(x,1),0,1)
+# curve(invExpCDF(x,alpha=1),0,1) #Same as qexp(x,alpha)
+
+# #fat tailed - difficult due to fact that function has no definite integral
+# ft <- function(x,alpha,beta) 1/(1+(alpha*x^beta))
+# invFt <- function(x,alpha,beta) ((1-x)/(alpha*x))^(1/beta)
+# 
+# ftPdf <- function(x,alpha,beta) ft(x,alpha,beta)/integrate(ft,0,Inf,alpha=alpha,beta=beta)$value
+# 
+# curve(ft(x,alpha=0.4,beta=1.6),0,100)
+# curve(invFt(x,alpha=0.4,beta=1.6),0,1)
+# curve(ftPdf(x,alpha=0.4,beta=1.6),0,100)
+
+
 #Function to generate distribution of propagules from some source
-
-
-mkDist <- function(patches,r,disp,T,kfac,propDisperse=1,figure=T,lastStep=F,returnDist=F){
+mkDist <- function(patches,worldLims,r,dispType,dispPars,tim,kfac,propDisperse=1,figure='hist',lastStep=F,returnDist=F){
   #Parameters for patches
   patches$size <- with(patches,xmax-xmin) #Size of patch
   patches$mid <- rowMeans(patches[,c('xmax','xmin')]) #Middle of patch
@@ -29,7 +43,7 @@ mkDist <- function(patches,r,disp,T,kfac,propDisperse=1,figure=T,lastStep=F,retu
   
   recordDisp <- list() #List to store dispersal
   
-  for(t in 1:T){
+  for(t in 1:tim){
     #Reproduce
     for(i in 1:npatches){
       patches$n[i] <- round(ntPlus(patches$n[i],r,patches$size[i]))
@@ -38,17 +52,22 @@ mkDist <- function(patches,r,disp,T,kfac,propDisperse=1,figure=T,lastStep=F,retu
     #Disperse
     
     # dLocs <- with(patches,unlist(mapply(rnorm,n,mid,disp))) #Disperse from middle of patch
-    dLocs <- numeric(0) #Disperse from random point within patch
+    dLocs <- numeric(0) #Empty set to store locations
     for(i in 1:npatches){
       #Dispersal location for migrants - all individuals disperse if propDisperse == 1
+      nDispersers <- round(patches$n[i]*propDisperse)
       
-      #Normal distribution
-      dLocs <- c(dLocs,with(patches,rnorm(round(n[i]*propDisperse),runif(n[i],xmin[i],xmax[i]),disp)))
-      
+      dLocs <- with(patches,c(dLocs,switch(dispType, #Choose dispersal kernel
+        normal=rnorm(nDispersers,runif(nDispersers,xmin[i],xmax[i]),dispPars[1]), #Normal distribution
+        cauchy=rcauchy(nDispersers,runif(nDispersers,xmin[i],xmax[i]),dispPars[1]), #Cauchy
+        t=rt(nDispersers,dispPars[2])*dispPars[1] + runif(nDispersers,xmin[i],xmax[i]) #Student's t (regularized)
+      )))
+                     
       #Should try fat-tailed as well, as in Chapman et al 2006
       #https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/j.1365-2656.2006.01172.x
       
-      patches$n[i] <- round(n[i]*(1-propDisperse)) #Remove migrants
+      patches$n[i] <- patches$n[i] - nDispersers #Remove migrants
+      if(patches$n[i]<0) stop('Population negative')
     }
     
     #Assign migrants to new patches
@@ -63,10 +82,16 @@ mkDist <- function(patches,r,disp,T,kfac,propDisperse=1,figure=T,lastStep=F,retu
   
   if(lastStep) recordDisp <- recordDisp[[length(recordDisp)]] #Only saves the last time step
   
-  if(figure){ #Make a figure of the dispersal data
+  recordDisp <- unlist(recordDisp) #Converts to vector
+  recordDisp <- recordDisp[recordDisp>=min(worldLims)&recordDisp<=max(worldLims)] #Get rid of records outside of world range
+  
+  if(figure=='hist'|figure=='dens'){ #Make a figure of the dispersal data
     # plot(0,type='n',xlim=range(patches[,c('xmax','xmin')]),ylim=c(0,max(patches$n)),xlab='',ylab='') #Empty plot
-    # plot(density(unlist(recordDisp)),zero.line=T,main='',xlim=range(patches[,c('xmax','xmin')])) #Density plot
-    hist(unlist(recordDisp),freq=F,main='',breaks=100,xlim=range(patches[,c('xmax','xmin')]))
+    if(figure=='hist') hist(unlist(recordDisp),freq=F,main='',breaks=100,xlim=worldLims,xlab='Distance') #Histogram plot
+    
+    if(figure=='dens') plot(density(unlist(recordDisp)),zero.line=T,main='',xlim=worldLims,xlab='Distance') #Density plot
+    
+    
     for(i in 1:npatches){
       with(patches,{
         lines(c(xmin[i],xmax[i]),c(0,0),col='forestgreen',lwd=3)
@@ -80,11 +105,23 @@ mkDist <- function(patches,r,disp,T,kfac,propDisperse=1,figure=T,lastStep=F,retu
 }
 
 patches <- data.frame(xmin=c(0,30,70),xmax=c(10,40,100),n=c(5,5,15))
+worldLims <- c(0,100)
 r <- 1 #Rate of growth
-disp <- 10 #SD of dispersal
-T <- 100 #Time steps
-kfac <- 30 #Factor to multiply k by (change carrying capacity for all patches)
 
-mkDist(patches,r,disp,T,kfac,lastStep=T,propDisperse=0.2)
+#normal distribution - to do:
+
+# #t distribution
+# dispType <- 't'
+# dispPars <- c(5,3) #dispersal parameters
+#cauchy distribution 
+dispType <- 'cauchy'
+dispPars <- c(1) #dispersal parameters
+
+tim <- 500 #Time steps
+kfac <- 100 #Factor to multiply k by (change carrying capacity for all patches)
+
+mkDist(patches,worldLims,r,dispType,dispPars,tim,kfac,lastStep=F,propDisperse=0.1)
+debugonce(mkDist)
+
 
 
