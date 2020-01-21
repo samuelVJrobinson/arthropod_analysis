855334699c365723473992aa211633b38b15978f
samuelVJrobinson <samuel.vj.robinson@gmail.com>
2020-01-20 17:29:50 -0700

    Finished simulation function.
    Started writing function to make "onion skin" coverages of patches.

--- dispersalSim.R
+++ dispersalSim.R
@@ -1,33 +1,42 @@
 #Simulation to look at dispersal distances from seminatural features, SR 2020
 
+library(tidyverse)
+
+# Set up simulation -------------------------------------------------------
+
 #Idea: 2D world of habitable patches surrounded by uninhabitable matrix. 
 #Organisms start in a patch, reproduce according to logistic growth equation (K related to size of patch), and then disperse according to some univariate normal distribution
 #If they land in another/the same patch, they add to its population
 #Basically just the theory of island biogeography + metapopulation stuff
 
-#Logistic growth equation (n at t+1)
-ntPlus <- function(nt,r,K){
-  nt+(r*nt*(1-(nt/K)))
-}
-
-# expCDF <- function(x,alpha) 1-exp(-alpha*x) #Exponential curve
-# invExpCDF <- function(x,alpha) -log(-x+1)/alpha
-# curve(qexp(x,1),0,1)
-# curve(invExpCDF(x,alpha=1),0,1) #Same as qexp(x,alpha)
-
-# #fat tailed - difficult due to fact that function has no definite integral
-# ft <- function(x,alpha,beta) 1/(1+(alpha*x^beta))
-# invFt <- function(x,alpha,beta) ((1-x)/(alpha*x))^(1/beta)
-# 
-# ftPdf <- function(x,alpha,beta) ft(x,alpha,beta)/integrate(ft,0,Inf,alpha=alpha,beta=beta)$value
-# 
-# curve(ft(x,alpha=0.4,beta=1.6),0,100)
-# curve(invFt(x,alpha=0.4,beta=1.6),0,1)
-# curve(ftPdf(x,alpha=0.4,beta=1.6),0,100)
-
-
 #Function to generate distribution of propagules from some source
+#patches - dataframe of patches: xmin, xmax, n = starting population
+#worldLims - vector of x-limits
+#r - rate of growth at n=0
+#dispType - dispersal kernel ('normal','t','cauchy','ft')
+#dispPars - vector of parameters for dispersal (SD, c(SD,nu) for t-dist, c(a,b) for fat-tailed)
+#tim - time steps to take
+#kfac - factor to multiply patch size by (raises K-constant of patch)
+#propDisperse - proportion of organisms that disperse, rather than staying in the patch (between 0 and 1)
+#figure - what type of figure should be displayed ('hist','dense','none')
+#lastStep - should only last step be saved (T/F)
+#returnDist - should locations of dispersers be returned as a vector (T/F)
 mkDist <- function(patches,worldLims,r,dispType,dispPars,tim,kfac,propDisperse=1,figure='hist',lastStep=F,returnDist=F){
+  #Helper functions
+  
+  #Logistic growth equation (n at t+1)
+  ntPlus <- function(nt,r,K){
+    nt+(r*nt*(1-(nt/K)))
+  }
+  
+  #Generate random numbers from 2-sided fat tailed distribution
+  #1-sided: function(d,a,b) 1/(1+a*d^b)
+  rft2 <- function(n,a,b){
+    d <- runif(n,0,1) #Random uniform
+    posneg <- sample(c(-1,1),n,T) #Positive/negative 1
+    return(posneg*((1-d)/(a*d))^(1/b))
+  }
+  
   #Parameters for patches
   patches$size <- with(patches,xmax-xmin) #Size of patch
   patches$mid <- rowMeans(patches[,c('xmax','xmin')]) #Middle of patch
@@ -60,10 +69,13 @@ mkDist <- function(patches,worldLims,r,dispType,dispPars,tim,kfac,propDisperse=1
       dLocs <- with(patches,c(dLocs,switch(dispType, #Choose dispersal kernel
         normal=rnorm(nDispersers,runif(nDispersers,xmin[i],xmax[i]),dispPars[1]), #Normal distribution
         cauchy=rcauchy(nDispersers,runif(nDispersers,xmin[i],xmax[i]),dispPars[1]), #Cauchy
-        t=rt(nDispersers,dispPars[2])*dispPars[1] + runif(nDispersers,xmin[i],xmax[i]) #Student's t (regularized)
+        t=rt(nDispersers,dispPars[2])*dispPars[1] + runif(nDispersers,xmin[i],xmax[i]), #Student's t (regularized)
+        ft=rft2(nDispersers,dispPars[1],dispPars[2]) + runif(nDispersers,xmin[i],xmax[i]) #2-sided fat-tailed
       )))
                      
       #Should try fat-tailed as well, as in Chapman et al 2006
+      #Formula f(d|a,b) = 1/(1+a*d^b); authors found a = 0.3-0.4, b = 1.5-1.6
+      #Turns out this is impossible to integrate. Better to use t dist or cauchy dist.
       #https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/j.1365-2656.2006.01172.x
       
       patches$n[i] <- patches$n[i] - nDispersers #Remove migrants
@@ -108,20 +120,103 @@ patches <- data.frame(xmin=c(0,30,70),xmax=c(10,40,100),n=c(5,5,15))
 worldLims <- c(0,100)
 r <- 1 #Rate of growth
 
-#normal distribution - to do:
+# #normal distribution
+# dispType <- 'normal'
+# dispPars <- c(5)
 
 # #t distribution
 # dispType <- 't'
 # dispPars <- c(5,3) #dispersal parameters
-#cauchy distribution 
-dispType <- 'cauchy'
-dispPars <- c(1) #dispersal parameters
 
-tim <- 500 #Time steps
-kfac <- 100 #Factor to multiply k by (change carrying capacity for all patches)
+# #cauchy distribution 
+# dispType <- 'cauchy'
+# dispPars <- c(1) #dispersal parameters
 
-mkDist(patches,worldLims,r,dispType,dispPars,tim,kfac,lastStep=F,propDisperse=0.1)
-debugonce(mkDist)
+#2-sided fat-tailed distribution
+dispType <- 'ft'
+dispPars <- c(0.4,1.5) #dispersal parameters
+
+tim <- 1000 #Time steps
+kfac <- 10 #Factor to multiply k by (change carrying capacity for all patches)
+
+mkDist(patches,worldLims,r,dispType,dispPars,tim,kfac,propDisperse=0.1,figure='hist',lastStep=F,returnDist=F)
+
+
+# Simple situation --------------------------------------------------------
+
+theme_set(theme_classic())
+
+#1 large patch on left side
+patches <- data.frame(xmin=c(0),xmax=c(20),n=c(5))
+worldLims <- c(0,100)
+r <- 1 #Rate of growth
+tim <- 2000 #Time steps
+kfac <- 10 #Factor to multiply k by
+
+#Simulate dispersal at 3 distances (25,50,75)
+dispDist <- lapply(1:100,function(x){
+  data.frame(d=mkDist(patches,worldLims,r,dispType='ft',dispPars=c(0.4,1.5),tim,kfac,propDisperse=0.1,figure='none',lastStep=F,returnDist=T)) %>% 
+    filter(d>20) %>% mutate(d=d-20) %>% #Removes dispersals within patch
+    # ggplot(aes(x=d))+geom_histogram()
+    mutate(d=case_when(d>20&d<30 ~ 'near', d>45&d<55 ~ 'mid', d>70&d<80 ~ 'far')) %>% filter(!is.na(d)) %>% 
+    mutate(d=factor(d,levels=c('near','mid','far'))) %>% group_by(d,.drop=F) %>% summarize(n=n()) %>% ungroup() %>% 
+    mutate(d=case_when(d=='near' ~ 25, d=='mid' ~ 50, d=='far' ~ 75)) %>% 
+    mutate(rep=x)
+})
+dispDist <- do.call('rbind',dispDist) %>% 
+  mutate(rep=factor(rep))
+
+mod1 <- dispDist %>% mutate(d=scale(d)) %>% 
+  glm(n~d,data=.,family='poisson')
+
+mod1 <- dispDist %>% mutate(logn=log(n+0.1),d=d) %>% 
+  lm(n~sqrt(d),data=.)
+
+# mod1 <- dispDist %>% mutate(d=scale(d)) %>% 
+#   glmer(n~d+(d|rep),data=.,family='poisson')
+summary(mod1)
+par(mfrow=c(2,1)); plot(mod1,which=c(1,2))
+
+
+
+#Function to extract "onion skin" proportions of patch cover at distances from cent
+# patchDat: dataframe with xlims of patches
+# cent: vector of centre locations
+# w: width of each slice
+# dist: max distance(s) to use
+
+onionSkin <- function(patchDat,cent,w,dist){
+  
+  patchDat <- data.frame(xmin=c(0),xmax=c(20),n=c(5))  
+  patchDat
+  
+  
+  #Number of onion slices (rounded down)
+  nslice <- dist %/% w
+  
+  results <- matrix(rep(NA,length(cent)*nslice),nrows=length(cent),ncol=nslice)
+  
+  for(i in 1:length(cent)){ #For each location
+    
+    
+    #Starting boundaries for each slice
+    left <- c(cent-w,cent)
+    right <- c(cent,cent+w)
+    
+    for(j in 1:nslice){ #For each slice
+      
+    }
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+}
 
 
 
