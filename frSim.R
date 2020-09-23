b27e6f4e4251b088571417bb03079bc8048dea64
samuelVJrobinson <samuel.vj.robinson@gmail.com>
2020-09-23 09:18:03 -0600

    Added ggplot example to frSim

--- frSim.R
+++ frSim.R
@@ -1,6 +1,10 @@
 #Simple example of functional regression
 #Sam Robinson, Spring 2020
 
+#Other things to try:
+# Fiddle around with variances
+# Fit only part of data (say, every 10 days)
+
 # Functions ---------------------------------------------------------------
 
 library(mgcv)
@@ -66,29 +70,66 @@ par(mfrow=c(1,1))
 yield <- ndviTrue %*% coefFun(1:100)
 
 
-# Fit model ---------------------------------------------------------------
+# Fit model and make simple plots ---------------------------------------------------------------
 
 #Create matrix of day values to use in model
 dayMat <- matrix(rep(1:Ndays,each=Nsamp),ncol=Ndays)
 
-#Fit model
-par(mfrow=c(2,2))
-
+#Model using thin-plate smoother
 mod1 <- gam(yield ~ s(dayMat,by=ndvi,bs='tp'))
+
+#Model using cubic regression smoother
+mod2 <- gam(yield ~ s(dayMat,by=ndvi,bs='cr'))
+
+#Check model output
+par(mfrow=c(2,2))
+gam.check(mod1)
+gam.check(mod2) #Both look OK
+
 summary(mod1)
 plot(mod1,shade=T,rug=F,ylab='Coefficient',xlab='Day',main='Thin-plate spline')
 lines(1:100,coefFun(1:100),col='red',lwd=2) #Pretty close fit
 legend('topright',c('Fit','Actual'),fill=c('black','red'))
 plot(predict(mod1),yield,xlab='Predicted',ylab='Actual'); abline(0,1,col='red')
 
-
-mod2 <- gam(yield ~ s(dayMat,by=ndvi,bs='cr'))
 summary(mod2)
 plot(mod2,shade=T,rug=F,ylab='Coefficient',xlab='Day',main='Cubic regression spline')
 lines(1:100,coefFun(1:100),col='red',lwd=2) #Similar fit from a cubic regression spline
 legend('topright',c('Fit','Actual'),fill=c('black','red'))
 plot(predict(mod1),yield,xlab='Predicted',ylab='Actual'); abline(0,1,col='red')
 
-#Other things to try:
-# Fiddle around with variances
-# Fit only part of data (say, every 10 days)
+
+#Create partial effect plot of functional regression term using ggplot2 ------------------
+
+library(ggplot2)
+theme_set(theme_classic())
+
+#Dataframe of predictors to smooth across (days 0:100 + column of 1s for "ndvi")
+(dat <- data.frame(dayMat=0:100,ndvi=1)) 
+
+#Get predictor matrix (X) for 1st smoother, using dat
+predMat <- PredictMat(mod1$smooth[[1]],data=dat) 
+
+#Locations of coefficients to pull from mod1 (coefs 2:11)
+(coefRange <- as.vector(sapply(mod1$smooth[1],function(x) x$first.para:x$last.para)))
+
+(coefs <- coef(mod1)[coefRange]) #Extract coefficient values from slots 2:11
+
+# Predicted value - predictor matrix X coefficients
+dat$pred <- predMat %*% coefs
+
+#SE for coefficients, basically just adds matrix products of covariance matrix * predicted values - "borrowed" from Wood's plot.gam code 
+dat$se <- sqrt(pmax(0,rowSums(predMat %*% mod1$Vp[coefRange,coefRange] * predMat)))
+
+#Confidence intervals
+ci <- 1.96 #Range of confidence intervals (Z-score)
+dat$upr <- dat$pred + dat$se*ci; dat$lwr <- dat$pred - dat$se*ci #Upper and lower ranges for each point
+  
+dat #Looks OK
+
+#Prediction lines up pretty well with actual
+ggplot(dat,aes(x=dayMat,y=pred))+geom_ribbon(aes(ymax=upr,ymin=lwr),alpha=0.3)+
+  geom_line()+ #Predicted relationship
+  #Plot actual relationship
+  geom_line(data=data.frame(x=1:100,y=coefFun(1:100)),aes(x=x,y=y),linetype='dashed',col='red')+
+  labs(x='Day of Year',y='Coefficient')
